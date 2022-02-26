8da245ea85372e539a642254847e5975b43564a4
Harshit Krishna <b20103@students.iitmandi.ac.in>
2022-02-26 20:07:59 +0530

    Logo

--- client/src/components/headers/light.js
+++ client/src/components/headers/light.js
@@ -6,7 +6,7 @@ import { css } from "styled-components/macro"; //eslint-disable-line
 
 import useAnimatedNavToggler from "../../helpers/useAnimatedNavToggler.js";
 
-import logo from "../../images/logo3.png";
+import logo from "../../images/1-removebg-preview.png";
 import { ReactComponent as MenuIcon } from "feather-icons/dist/icons/menu.svg";
 import { ReactComponent as CloseIcon } from "feather-icons/dist/icons/x.svg";
 

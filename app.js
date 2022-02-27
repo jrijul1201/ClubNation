bbf9eb7e883a3eebb7b35d1e9c85dbeedbeaec66
Rijul Jain <jrijul1201@gmail.com>
2022-02-27 21:12:27 +0530

    git port path

--- app.js
+++ app.js
@@ -40,14 +40,9 @@ app.use('/stat',statsRouter);
 
 const PORT = process.env.PORT || 5000;
 
-if(process.env.NODE_ENV === 'production') {
-   
-    app.use(express.static(path.join(__dirname, "client", "build")))
-  
-    app.get('*',(req, res) => {
-        res.sendFile(path.join(__dirname,'client','build','index.html'));
-    });
-  }
+if (process.env.NODE_ENV==='production') {
+    app.use(express.static('client/build'));
+}
 app.listen(PORT,()=>{
     console.log('Express Server Started');
 });

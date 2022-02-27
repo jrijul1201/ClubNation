c6259ffeb63d4078c959ed57f9291a02dd3209bf
Rijul Jain <jrijul1201@gmail.com>
2022-02-27 21:03:08 +0530

    jeewan barbaad h

--- app.js
+++ app.js
@@ -40,9 +40,14 @@ app.use('/stat',statsRouter);
 
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV==='production') {
-    app.use(express.static('client/build'));
-}
+if(process.env.NODE_ENV === 'production') {
+   
+    app.use(express.static(path.join(__dirname, "client", "build")))
+  
+    app.get('*',(req, res) => {
+        res.sendFile(path.join(__dirname,'client','build','index.html'));
+    });
+  }
 app.listen(PORT,()=>{
     console.log('Express Server Started');
 });
